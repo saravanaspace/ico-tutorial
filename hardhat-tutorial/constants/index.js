ec5b2b6b028a91c0c8be89302252aac26c3e6ae1
Saravana <saravana@h2i.sg>
2022-08-09 17:56:19 +0800

    first commit for ICO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xaC2E952eca5B8AC031870ACd733eE4c1b2492E4A";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
